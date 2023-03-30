077cf9ccc440b4e6495ee8de805442c30ded97dd
Gameplushy <victor_f@orange.fr>
2023-03-30 16:37:31 +0200

    3-letter morse

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,36 @@ function App() {
   const [ledToggler3,toggleLed3] = useState("")
 
   const [ganttSpans,setSpans] = useState([])
+  const [morsePlainText,setMorseTest] = useState("")
+
+  const morseDic = {
+    "A":".-",
+    "B":'-...',
+    "C":"-.-.",
+    "D":"-..",
+    "E":".",
+    "F":"..-.",
+    "G":"--.",
+    "H":"....",
+    "I":"..",
+    "J":".---",
+    "K":"-.-",
+    "L":".-..",
+    "M":"--",
+    "N":"-.",
+    "O":"---",
+    "P":".--.",
+    "Q":"--.-",
+    "R":".-.",
+    "S":"...",
+    "T":"-",
+    "U":"..-",
+    "V":"...-",
+    "W":".--",
+    "X":"-..-",
+    "Y":"-.--",
+    "Z":"--.."
+  }
 
   function ConnectToBluetooth(){
     if (!('bluetooth' in navigator)) {
@@ -90,10 +120,53 @@ function Connect() {
     //ENVOYER ONELINE
   }
 
+  function SendMorse(){
+    if(!morsePlainText)
+      return;
+    if(morsePlainText.length==3){
+      var array = morsePlainText.toUpperCase().split('')
+      var commands = "";
+      for (let i = 0; i < array.length; i++) {
+        const c = array[i];
+        var j = i+1;
+        var timer=0
+        var code = morseDic[c]
+        code.split('').forEach(d=>{
+          var line = j+" "+timer;
+          timer += d=='.'?300:700;
+          line+=" "+timer+"\r\n"
+          timer+=100;
+          commands+=line
+        })
+        timer+=200
+      }
+      commands = commands.substring(0,commands.length-2)
+      MorseFlash(commands)
+      MorseGantt(commands)
+    }
+  }
+
   function Flash(){
     var flashFunctions = [toggleLed1,toggleLed2,toggleLed3]
     var numberOfFlashAsks = [0,0,0]
     script.split(/\r\n|\r|\n/).forEach(flash=>{
+      var flashArray = flash.split(" ");
+      flashArray[0] = flashArray[0]-1
+      setTimeout(() => {
+        numberOfFlashAsks[flashArray[0]]++
+        if(numberOfFlashAsks[flashArray[0]]==1)flashFunctions[flashArray[0]]("ledon");
+        setTimeout(() => {
+          numberOfFlashAsks[flashArray[0]]--
+          if(numberOfFlashAsks[flashArray[0]]==0)flashFunctions[flashArray[0]]("");
+        }, flashArray[2]-flashArray[1]);
+      }, flashArray[1]);
+    })
+  }
+
+  function MorseFlash(line){
+    var flashFunctions = [toggleLed1,toggleLed2,toggleLed3]
+    var numberOfFlashAsks = [0,0,0]
+    line.split(/\r\n|\r|\n/).forEach(flash=>{
       var flashArray = flash.split(" ");
       flashArray[0] = flashArray[0]-1
       setTimeout(() => {
@@ -122,10 +195,27 @@ function Connect() {
     })
   }
 
+  function MorseGantt(line){
+    setSpans([])
+    var commands = line.split(/\r\n|\r|\n/)
+    console.log(commands)
+    var endTime = Math.max(...commands.map(t=>parseInt(t.split(" ")[2])))
+    commands.forEach(cmd => {
+      var value = {top: 0, left: 0, size: 0};
+      cmd = cmd.split(" ")
+      value.top = ((cmd[0])-1)*(100/3)
+      value.left = (((cmd[1])/endTime)*100)
+      value.size = (((cmd[2]-cmd[1])/endTime)*100)
+      //var value = "{top : "+top+"%; left : "+left+"%; width : "+size+"%}"
+      setSpans(ganttSpans=>[...ganttSpans,value])
+    })
+  }
+
   return (
     <div className="App">
       <div><button onClick={ConnectToBluetooth}>Connect to Bluetooth</button></div>
       <div><textarea value={script} onChange={e=>setScript(e.target.value)} cols="20" rows="20"></textarea></div>
+      <div><input type="text" value={morsePlainText} onChange={e=>setMorseTest(e.target.value)}/></div>
       <div id="ledrow">
         <span className={"led "+ledToggler1}>1</span>
         <span className={"led "+ledToggler2}>2</span>
@@ -134,7 +224,10 @@ function Connect() {
       <div id="ganttContainer">
         {ganttSpans.length!=0 ? ganttSpans.map(s=><span className='ganttBar' style={{'top': s.top+'%', 'left': s.left+'%', 'width': s.size+'%'}}></span>) : ""}
       </div>
-      <div><button onClick={SendScript}>Send</button></div>
+      <div>
+        <button onClick={SendScript}>Send script</button>
+        <button onClick={SendMorse}>Send Morse</button>
+      </div>
     </div>
   )
 }
