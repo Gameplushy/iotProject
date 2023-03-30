83068c43b132b152ff6b33d97154cdcf172bce65
Gameplushy <victor_f@orange.fr>
2023-03-30 16:52:23 +0200

    Remove comments

--- src/App.jsx
+++ src/App.jsx
@@ -62,48 +62,6 @@ function App() {
       .catch((err) => {
         console.log('an error occured : ' + err.message)
       })
-    // .then(function (server) {
-    //   console.log(server)
-    //   return server.getPrimaryService('00002a37-0000-1000-8000-00805f9b34fb');
-    // })
-    // .then(function (service) {
-    //   console.log(service)
-    //   return service.getCharacteristic('4af9208bab88');
-    // })
-    // .then(function (characteristic) {
-    //   return characteristic.readValue();
-    // })
-    // .then(function (value) {
-    //   changeRes('Battery percentage is ' + value.getUint8(0) + '.');
-    // })
-    // .catch(function (error) {
-    //   changeRes(error.message);
-    //   // setTimeout(() => {
-    //   //   changeRes("")
-    //   // }, 3000);
-    // });
-  }
-
-  // // A function that will be called once got characteristics
-  // function gotCharacteristics(error, characteristics) {
-  //   if (error || !characteristics){
-  //     console.log("error: ", error);
-  //     return;
-  //   } 
-  //   console.log("characteristics: ", characteristics);
-  //   myCharacteristic = characteristics['4af9208bab88'];
-  //   // Read the value of the first characteristic
-  //   myBLE.read(myCharacteristic, gotValue);
-  // }
-
-  // // A function that will be called once got values
-  // function gotValue(error, value) {
-  //   if (error) console.log("error: ", error);
-  //   console.log("value: ", value);
-  //   myValue = value;
-  //   // After getting a value, call p5ble.read() again to get the value again
-  //   myBLE.read(myCharacteristic, gotValue);
-  // }
 
   async function SendScript() {
     if (!bluetoothDevice) {
@@ -248,5 +206,5 @@ function App() {
     </div>
   )
 }
-
+}
 export default App
